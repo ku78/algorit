3dd809e230fdfd6663a7bbaee6fb606ada3d7c31
ku78 <60285617+ku78@users.noreply.github.com>
2021-07-23 21:05:26 +0300

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,1 +1,14 @@
+package com.company;
 
+public class Main {
+    public static void main(String[] args) {
+        LinearProbingHashMap<Integer, String> lphm = new LinearProbingHashMap<>();
+
+        lphm.put(1, "one");
+        lphm.put(555, "555e");
+        lphm.put(7878, "7878e");
+
+        System.out.println(lphm.get(555));
+
+    }
+}
